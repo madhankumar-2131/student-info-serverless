d2e5b981690b3e077a126e8c8a89f111a2e26d09
MADHANKUMAR S <madhan19072004@gmail.com>
2025-04-16 00:08:58 +0530

    first commit

---
+++ AWS-SERVERLESS-DEPLOYMENT-main/scripts.js
@@ -0,0 +1,50 @@
+var API_ENDPOINT = "https://mfg3guw5n7.execute-api.ap-south-1.amazonaws.com/prod";
+
+
+// AJAX GET request to retrieve all students
+document.getElementById("getstudents").onclick = function () {
+    $.ajax({
+        url: API_ENDPOINT,
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            var students = typeof response === "string" ? JSON.parse(response) :
+                response.body ? JSON.parse(response.body) : response;
+
+            $('#studentTable tr').slice(1).remove();
+            jQuery.each(students, function (i, data) {
+                $("#studentTable").append("<tr> \
+                    <td>" + data['studentid'] + "</td> \
+                    <td>" + data['name'] + "</td> \
+                    <td>" + data['class'] + "</td> \
+                    <td>" + data['age'] + "</td> \
+                    </tr>");
+            });
+        },
+        error: function () {
+            alert("Error retrieving student data.");
+        }
+    });
+};
+
+// AJAX POST request to save student data
+document.getElementById("savestudent").onclick = function () {
+    var inputData = {
+        "studentid": $('#studentid').val(),
+        "name": $('#name').val(),
+        "class": $('#class').val(),
+        "age": $('#age').val()
+    };
+    $.ajax({
+        url: API_ENDPOINT,
+        type: 'POST',
+        data: JSON.stringify(inputData),
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            document.getElementById("studentSaved").innerHTML = "Student Data Saved!";
+        },
+        error: function () {
+            alert("Error saving student data.");
+        }
+    });
+};
